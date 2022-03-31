841d4f0aa59a86014f491a3dd843e6e9f983e683
RustyNox <noelmdixon@gmail.com>
2022-03-31 19:03:52 +1100

    Getting the path name Also added regex to clean up path e.g. /foo/bar/

--- index.js
+++ index.js
@@ -6,10 +6,25 @@
 
 // dependencies
 const http = require('http')
+const url = require('url')
 
 // the server should respond to all request with a string
 const server = http.createServer(function(req, res){
+
+  // get the url and parse interval
+  const parsedUrl = url.parse(req.url, true)
+
+  // get the path from the url
+  const path = parsedUrl.pathname
+  const trimmedPath = path.replace(/^\/+|\/+$/g,'')
+
+  // send the response
   res.end('Hello Node World\n')
+
+  // log the request path
+  console.log(`Request received on path: ${trimmedPath}`)
+
+
 })
 
 // start the server, and have it listen on port 3000
